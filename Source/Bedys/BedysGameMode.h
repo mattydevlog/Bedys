af017a172e70a495e331c88e22985b97e006bbd7
WilliamArnberg <arnberg19@gmail.com>
2022-11-22 20:59:04 +0100

    BaseClassesCreated
    BaseClasses created and overall structure

---
+++ Source/Bedys/BedysGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BedysGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BEDYS_API ABedysGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Bedys/BoosterPickup.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePickup.h"
+#include "BoosterPickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BEDYS_API ABoosterPickup : public ABasePickup
+{
+	GENERATED_BODY()
+	
+};
